0a85440f748b6f7a23d58eeaa3247ec544ad77e0
lee3ll <125418017+lee3ll@users.noreply.github.com>
2023-05-01 18:05:50 +0900

    ff

--- javascript/game/js/music.js
+++ javascript/game/js/music.js
@@ -60,12 +60,17 @@ const musicPlay= musicWrap.querySelector("#control-play");
 const musicPrevBtn=musicWrap.querySelector("#control-prev");
 const musicNextBtn=musicWrap.querySelector("#control-next");
 const musicProgress = musicWrap.querySelector(".progress");
-const musicProgressBar = musicWrap.querySelector(".progress .bar")
-const musicProgressCurrent = musicWrap.querySelector(".music__contents .timer .current")
-const musicProgressDuration = musicWrap.querySelector(".music__contents .timer .duration")
+const musicProgressBar = musicWrap.querySelector(".progress .bar");
+const musicProgressCurrent = musicWrap.querySelector(".music__contents .timer .current");
+const musicProgressDuration = musicWrap.querySelector(".music__contents .timer .duration");
+const musicRepeat = musicWrap.querySelector("#control-repeat");
+const musicListBtn = musicWrap.querySelector("#control-list")
+const musicList = musicWrap.querySelector(".music__list");
+const musicListUl = musicWrap.querySelector(".music__list ul");
+const musicListClose = musicWrap.querySelector(".music__list h3 .close");
 // const musicPlay = 
 
-let musicIndex = 10; //현재 음악 인덱스
+let musicIndex = 5; //현재 음악 인덱스
 
 
 // 음악 재생
@@ -110,6 +115,7 @@ const prevMusic = () => {
 
     loadMusic(musicIndex);
     playMusic();
+    playListMusic();
 }
 
 //다음 곡 듣기
@@ -121,6 +127,7 @@ const nextMusic = () => {
 
     loadMusic(musicIndex);
     playMusic();
+    playListMusic();
 }
 //뮤직 진행바
 musicAudio.addEventListener("timeupdate", e => {
@@ -159,6 +166,52 @@ musicProgress.addEventListener("click", (e) => {
     console.log(songDuration)
 });
 
+//반복버튼 클릭
+musicRepeat.addEventListener("click",() => {
+    let getAttr = musicRepeat.getAttribute("class");
+
+    switch(getAttr){
+        case "repeat":
+            musicRepeat.setAttribute("class", "repeat_one");
+            musicRepeat.setAttribute("title", "한곡 반복");
+        break;
+        case "repeat_one":
+            musicRepeat.setAttribute("class", "shuffle");
+            musicRepeat.setAttribute("title", "랜덤 반복");
+        break;
+        case "shuffle" :
+            musicRepeat.setAttribute("class","repeat");
+            musicRepeat.setAttribute("title","전체 반복");
+        break;
+    }
+});
+
+//오디오가 끝나면
+musicAudio.addEventListener("ended", () => {
+    let getAttr = musicRepeat.getAttribute("class");
+
+    switch(getAttr){
+        case "repeat" :
+            nextMusic();
+        break;
+        case "repeat_one" :
+            playMusic();
+        break;
+        case "shuffle" :
+            let randomIndex = Math.floor(Math.random() * allMusic.length + 1); //랜덤 인덱스 생성
+
+            do {
+                randomIndex = Math.floor(Math.random() * allMusic.length + 1);
+            } while (musicIndex == randomIndex);
+
+            musicIndex = randomIndex;   //현재 인덱스를 랜덤 인덱스로 변경
+            loadMusic(randomIndex);
+            playMusic();
+        break;
+    }
+    playListMusic();
+});
+
 //플레이 버튼 클릭
 musicPlay.addEventListener("click", () => {
     const isMusicPaused = musicWrap.classList.contains("paused");   //음악 재생중
@@ -176,6 +229,48 @@ musicNextBtn.addEventListener("click", () => {
     nextMusic();
 });
 
+//뮤직 리스트 버튼
+musicListBtn.addEventListener("click", () => {
+    musicList.classList.add("show");
+});
+
+musicListClose.addEventListener("click", () => {
+    musicList.classList.remove("show");
+});
+
+//뮤직 리스트 구현하기
+for(let i=0; i<allMusic.length; i++){
+    let li = `
+        <li data-index="${i+1}">
+            <span class="img">
+                <img class="img" src="img/${allMusic[i].img}" alt="${allMusic[i].name}">
+            </span>
+            <span class="title">
+                <strong>${allMusic[i].name}</strong>
+                <em>${allMusic[i].artist}</em>
+                <audio class="${allMusic[i].audio}" src="audio/${allMusic[i].audio}.mp3"></audio>
+            </span>
+            <span class="audio-duration" id="${allMusic[i].audio}"}>3:04</span>
+        </li>
+    `;
+        
+    //musicListUl.innerHTML += li;  //beforeend와 같음
+    musicListUl.insertAdjacentHTML("beforeend", li);
+
+    // 리스트에 음악 시간 불러오기
+    let liAudioDuration = musicListUl.querySelector(`#${allMusic[i].audio}`);     //리스트에서 시간을 표시할 선택자
+    let liAudio = musicListUl.querySelector(`.${allMusic[i].audio}`);             //리스트에서 오디오 파일 선택
+    liAudio.addEventListener("loadeddata", ()=> {
+        let audioDuration = liAudio.duration;
+
+        let totalMin = Math.floor(audioDuration / 60);
+        let totalSec = Math.floor(audioDuration % 60);
+        if(totalSec < 10) totalSec = `0${totalSec}`;
+        liAudioDuration.innerText = `${totalMin}:${totalSec}`;
+        liAudioDuration.setAttribute("data-duration", `${totalMin}:${totalSec}`);
+    });
+}
+
 // //음악 재생
 // const loadMusic = (num) => {
 //     musicName.innerText = allmusic[num-1].name;             //뮤직 이름
@@ -185,7 +280,39 @@ musicNextBtn.addEventListener("click", () => {
 //     musicAudio.src = `audio/${allmusic[num-1].audio}.mp3`   //뮤직파일
 // }
 
+//뮤직 리스트를 클릭하면 재생
+function playListMusic(){
+    const musicListAll = musicListUl.querySelectorAll("li");    //뮤직 리스트 목록
+    
+    for(let i=0; i<musicListAll.length; i++){
+        let audioTag = musicListAll[i].querySelector(".audio-duration");
+
+        musicListAll[i].setAttribute("onclick", "clicked(this)");
+
+        if(musicListAll[i].classList.contains("playing")){
+            musicListAll[i].classList.remove("playing");
+            let dataAudioDuration = audioTag.getAttribute("data-duration");
+            audioTag.innerText = dataAudioDuration;
+        }
+
+        if(musicListAll[i].getAttribute("data-index") == musicIndex){
+            musicListAll[i].classList.add("playing");
+            audioTag.innerText = "재생중";
+        }
+    }
+}
+
+//뮤직 리스트를 클릭하면
+function clicked(el){
+    let getIndex = el.getAttribute("data-index");
+    // alert(getIndex);
+    musicIndex = getIndex;
+    loadMusic(musicIndex);
+    playMusic();
+    playListMusic();
+}
+
 window.addEventListener("load", ()=>{
     loadMusic(musicIndex);
-    console.log(musicIndex);
+    playListMusic();
 });
